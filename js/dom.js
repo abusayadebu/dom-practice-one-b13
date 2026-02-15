786781a2eb1b6090487a88486cce84a6b7ee527d
abusayadebu <abusayad743@gmail.com>
2026-02-15 15:57:52 +0600

    style by dom in html element

--- js/dom.js
+++ js/dom.js
@@ -16,16 +16,23 @@ for (const p of pCollection){
 // getElement by Id
 const bdh1 = document.getElementById("bd-h-1")
 bdh1.innerText = "Bangladesh Updated"
+bdh1.style.color = 'red';
 
 const seasons = document.getElementById("seasons")
 // console.log(seasons);
 // seasons.innerText = "season is gone"
 seasons.innerHTML = `<h2>Hello seasons you are changed</h2>`
+seasons.style.backgroundColor = "yellow"
+seasons.style.border = "1px, solid"
+seasons.style.padding = "10px";
+seasons.style.borderRadius = '10px'
 
 
 // get element by className
 const books = document.getElementsByClassName("books")
 // console.log(books);
 for(const book of books){ 
-    console.log(book.innerText);
+    book.style.backgroundColor = "red";
+    book.style.padding = "10px"
+    book.style.borderRadius = "15px"
 }      
